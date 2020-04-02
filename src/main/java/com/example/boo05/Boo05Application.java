532ce6a69ba0e32d86c53296af240159bfce7614
孙亦航 <3388652256@qq.com>
2020-04-02 16:58:47 +0800

    这是第一次提交

---
+++ src/main/java/com/example/boo05/Boo05Application.java
@@ -0,0 +1,13 @@
+package com.example.boo05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boo05Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boo05Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boo05/Boo05ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boo05;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boo05ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
